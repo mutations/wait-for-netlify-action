a79a85b44edfd0a03ae10ef0161449d3c52c6319
Don Morrison <don@mutations.ltd>
2022-02-10 16:39:51 -0800

    Add increment parameter to waitForUrl
    Adds the `increment` parameter to the `waitForUrl` function
    to facilitate modifying the time used for the `setTimeout` call.

--- src/wait-for-url/wait-for-url.js
+++ src/wait-for-url/wait-for-url.js
@@ -1,8 +1,8 @@
 import * as core from '@actions/core'
 import axios from 'axios'
 
-const waitForUrl = async (url, MAX_TIMEOUT) => {
-  const iterations = MAX_TIMEOUT / 3
+const waitForUrl = async (url, MAX_TIMEOUT = 60, increment = 3) => {
+  const iterations = MAX_TIMEOUT / increment
   for (let i = 0; i < iterations; i++) {
     try {
       await axios.get(url)
@@ -10,7 +10,7 @@ const waitForUrl = async (url, MAX_TIMEOUT) => {
       return
     } catch (e) {
       console.log(`URL ${url} unavailable, retrying...`)
-      await new Promise((r) => setTimeout(r, 3000))
+      await new Promise((r) => setTimeout(r, increment * 1000))
     }
   }
   core.setFailed(`Timeout reached: Unable to connect to ${url}`)
