bcb5cd2f6382930d78d1736e79b59146ad408253
Don Morrison <don@mutations.ltd>
2022-02-13 00:25:25 -0800

    First-pass refactor of waitForUrl
    Refactors the internals of `waitForUrl` to use p-retry. The
    p-retry library:
    
    * Has a more straightforward API than `setInterval`/`setTimer`
    * Uses exponential backoff for the retry semantics
    * Allows for further refinement of the operations
    * Makes the first request immediately which can shorten run times when
      the resource is already available.
    
    The tests have been updated to more accurately reflect how Axios
    behaves in certain circumstances - errors for non-200 response.
    
    Refer to:
      * https://github.com/sindresorhus/p-retry
      * https://github.com/tim-kos/node-retry

--- src/wait-for-url/wait-for-url.test.js
+++ src/wait-for-url/wait-for-url.test.js
@@ -7,10 +7,8 @@ jest.mock('@actions/core')
 
 // URL doesn't matter since the call is mocked
 const url = 'https://some-url'
-// Shorten the setTimeout increment during test
-const timeoutIncrement = 0.25
 // Shorten the timeout during test
-const maxTimeout = 0.5
+const maxTimeout = 0.25
 
 describe('waitForUrl', () => {
   it('succeeds when the URL is found', async () => {
@@ -22,12 +20,16 @@ describe('waitForUrl', () => {
   })
 
   it('fails after reaching the max timeout when the URL is not found', async () => {
-    axios.get.mockImplementation(() => Promise.reject('not-found'))
+    axios.get.mockImplementation(() => {
+      // Axios throws errors for non 200 status responses
+      const notFound = new Error('Request failed with status code 404')
+      notFound.response = { status: 404 }
+
+      throw notFound
+    })
 
     // Promise resolves in this case, `setFailed` is the indication of failure
-    await expect(
-      waitForUrl(url, maxTimeout, timeoutIncrement),
-    ).resolves.toEqual()
+    await expect(waitForUrl(url, maxTimeout)).resolves.toEqual()
 
     expect(axios.get).toHaveBeenCalledWith(url)
     expect(core.setFailed).toHaveBeenCalledWith(
